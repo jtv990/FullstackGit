6f76f560b4564857b57f8f182e7f848d425bba3e
JV <109469126+jtv990@users.noreply.github.com>
2025-01-29 20:00:05 +0200

    unicafe:step2 done

--- Fullstackopen/Reactin_alkeet/osa1/src/App.jsx
+++ Fullstackopen/Reactin_alkeet/osa1/src/App.jsx
@@ -1,21 +1,37 @@
 import { useState } from 'react'
 
 const App = () => {
-  
-  const [good, setGood] = useState[0]
-  const [neutral, setNeutral] = useState[1]
-  const [bad, setBad] = useState[2]
 
-  
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const total = good + neutral + bad
+  const positivePercentage = total == 0 ? 0 : (good/total)*100
+  const average = total === 0 ? 0 : (good-bad)/total
+
+
   return (
     <div>
+      <h1>Palautenappi:</h1>
+      <button onClick={() => setGood(good + 1)}>
+        Hyvä {good}
+      </button>
+      <button onClick={() => setNeutral(neutral + 1)}>
+        OK {neutral}
+      </button>
+      <button onClick={() => setBad(bad + 1)}>
+        Huono {bad}
+      </button>
 
+      <p>Hyvä: {good}</p>
+      <p>OK: {neutral}</p>
+      <p>Huono: {bad}</p>
+      <p>Yhteensä: {total}</p>
+      <p>Keskiarvo: {average}</p>
+      <p>Positiivinen palaute: {positivePercentage.toFixed(2)}%</p>
     </div>
   )
 }
-
-
-
-
 
 export default App
