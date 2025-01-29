fd3f9aeef4ac16155167d7c64b56d8272573eefd
JV <109469126+jtv990@users.noreply.github.com>
2025-01-29 16:54:50 +0200

    tehtävät 1.3-1.5 done

--- Fullstackopen/Reactin_alkeet/osa1/src/main.jsx
+++ Fullstackopen/Reactin_alkeet/osa1/src/main.jsx
@@ -1,3 +1,3 @@
 import ReactDOM from 'react-dom/client'
-import App from './tehtavat1.1_ja_1.2'
+import App from './tehtava_osa1'
 ReactDOM.createRoot(document.getElementById('root')).render(<App />)
