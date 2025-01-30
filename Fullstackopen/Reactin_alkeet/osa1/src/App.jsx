ea91dd951acf0e3dab4818a58822a8a85dcc8789
JV <109469126+jtv990@users.noreply.github.com>
2025-01-30 17:42:50 +0200

    unicafe tehtävä valmis

--- Fullstackopen/Reactin_alkeet/osa1/src/App.jsx
+++ Fullstackopen/Reactin_alkeet/osa1/src/App.jsx
@@ -1,36 +1,51 @@
 import { useState } from 'react'
 
+const Button = ({onClick, text}) => (
+  <button onClick={onClick}>{text}</button>
+)
+
+const StatisticLine = ({text, value}) => (
+  <p>{text}: {value}</p>
+)
+
+const Statistics = ({good,neutral,bad}) => {
+  
+  const total = good + neutral + bad
+  const positivePercentage = total == 0 ? 0 : (good/total)*100
+  const average = total === 0 ? 0 : (good-bad)/total
+  
+  if (total === 0){
+    return <p>Palautetta ei ole annettu</p>
+  }
+  
+  return (
+    <div>
+      <p>Tilastot:</p>
+      <StatisticLine text="Hyvä" value ={good} />
+      <StatisticLine text="OK" value ={neutral} />
+      <StatisticLine text="Huono" value ={bad} />
+      <StatisticLine text="Yhteensä" value ={total} />
+      <StatisticLine text="Keskiarvo" value ={average} />
+      <StatisticLine text="Positiivinen palaute" value ={positivePercentage.toFixed(2)} />
+    </div>
+  )
+}
+
 const App = () => {
 
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
-  const total = good + neutral + bad
-  const positivePercentage = total == 0 ? 0 : (good/total)*100
-  const average = total === 0 ? 0 : (good-bad)/total
-
-
   return (
     <div>
-      <h1>Palautenappi:</h1>
-      <button onClick={() => setGood(good + 1)}>
-        Hyvä {good}
-      </button>
-      <button onClick={() => setNeutral(neutral + 1)}>
-        OK {neutral}
-      </button>
-      <button onClick={() => setBad(bad + 1)}>
-        Huono {bad}
-      </button>
-
-      <p>Hyvä: {good}</p>
-      <p>OK: {neutral}</p>
-      <p>Huono: {bad}</p>
-      <p>Yhteensä: {total}</p>
-      <p>Keskiarvo: {average}</p>
-      <p>Positiivinen palaute: {positivePercentage.toFixed(2)}%</p>
-    </div>
+    
+    <h1>Palautenappi:</h1>
+    <Button onClick={() => setGood(good + 1)} text="Hyvä" />
+    <Button onClick={() => setNeutral(neutral + 1)} text="OK" />
+    <Button onClick={() => setBad(bad + 1)} text="Huono" />
+    <Statistics good={good} neutral={neutral} bad={bad} />
+  </div>
   )
 }
 
