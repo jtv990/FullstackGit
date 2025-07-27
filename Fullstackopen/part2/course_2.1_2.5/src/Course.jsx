c3cb736db007822150a6bb40cfff174b3c39a6dc
JV <109469126+jtv990@users.noreply.github.com>
2025-07-27 15:51:09 +0300

    part2 exercises 2.1-2.5 done

---
+++ Fullstackopen/part2/course_2.1_2.5/src/Course.jsx
@@ -0,0 +1,28 @@
+const Header = ({ name }) => <h2>{name}</h2>
+
+const Part = ({ part }) => (
+  <p>
+    {part.name} {part.exercises}
+  </p>
+)
+
+const Content = ({ parts }) => (
+  <div>
+    {parts.map(part => <Part key={part.id} part={part} />)}
+  </div>
+)
+
+const Course = ({ course }) => {
+  const total = course.parts.reduce((sum, part) => sum + part.exercises, 0)
+
+  return (
+    <div>
+      <Header name={course.name} />
+      <Content parts={course.parts} />
+      <p><strong>total of {total} exercises</strong></p>
+    </div>
+  )
+}
+
+
+export default Course
